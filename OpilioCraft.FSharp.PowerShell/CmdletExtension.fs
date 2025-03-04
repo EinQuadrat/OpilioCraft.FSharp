a87c37d05f10974dea5731c0e931524b65d82934
Carsten Schäfer <carsten.schaefer@mailbox.org>
2025-03-04 19:21:23 +0100

    Integrated former separate repository OpilioCraft.FSharp.PowerShell.

---
+++ OpilioCraft.FSharp.PowerShell/CmdletExtension.fs
@@ -0,0 +1,24 @@
+﻿module OpilioCraft.FSharp.PowerShell.CmdletExtension
+
+open System.Management.Automation
+open ExceptionExtension
+
+type System.Management.Automation.PSCmdlet with
+    
+    // emit warnings
+    member x.WarnIfNone warning = fun o -> (if o |> Option.isNone then x.WriteWarning warning) ; o
+    member x.WarnIfFalse warning value = (if not value then x.WriteWarning warning) ; value
+
+    // simplify error handling
+    member x.WriteAsError errorCategory (exn : #System.Exception) : unit =
+        exn.ToError(errorCategory, x)
+        |> x.WriteError
+
+    member x.ThrowAsTerminatingError errorCategory (exn : #System.Exception) =
+        exn.ToError(errorCategory, x)
+        |> x.ThrowTerminatingError
+
+    member x.ThrowValidationError errorMessage errorCategory =
+        errorMessage
+        |> ParameterBindingException
+        |> x.ThrowAsTerminatingError errorCategory
